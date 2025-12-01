bd2f1f6272685029aabd243e360380c25ec3e16c
Abdallahyass <abdallahaahmad2000@gmail.com>
2025-12-01 21:17:50 +0300

    Fix web icons and images

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,58 +1,58 @@
 import 'dart:convert';
-import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 
 class AuthService {
-  // رابط السيرفر العالمي
+  // رابط السيرفر
   final String _baseUrl = 'https://filo-menu.onrender.com/api/auth';
 
-  // 1. دالة إنشاء حساب جديد
+  // 🔐 مفتاح الحماية (نفس اللي حطيته في Render)
+  final String _apiKey = 'FiloSecretKey202512341234';
+
+  // 1. تسجيل حساب جديد
   Future<bool> register(String name, String email, String password) async {
     try {
       final response = await http.post(
         Uri.parse('$_baseUrl/register'),
-        headers: {'Content-Type': 'application/json'},
+        headers: {
+          'Content-Type': 'application/json',
+          'x-api-key': _apiKey, // 👈 أضفنا هذا السطر المهم
+        },
         body: jsonEncode({'name': name, 'email': email, 'password': password}),
       );
 
       if (response.statusCode == 201) {
-        return true; // تم الإنشاء بنجاح
+        return true;
       } else {
-        if (kDebugMode) {
-          print('Register Error: ${response.body}');
-        }
+        // طباعة سبب الخطأ في الترمينال لنعرفه
+        print('Register Error: ${response.body}');
         return false;
       }
     } catch (e) {
-      if (kDebugMode) {
-        print('Error: $e');
-      }
+      print('Connection Error: $e');
       return false;
     }
   }
 
-  // 2. دالة تسجيل الدخول
+  // 2. تسجيل الدخول
   Future<bool> login(String email, String password) async {
     try {
       final response = await http.post(
         Uri.parse('$_baseUrl/login'),
-        headers: {'Content-Type': 'application/json'},
+        headers: {
+          'Content-Type': 'application/json',
+          'x-api-key': _apiKey, // 👈 وأضفناه هنا أيضاً
+        },
         body: jsonEncode({'email': email, 'password': password}),
       );
 
       if (response.statusCode == 200) {
-        // هنا المفروض نحفظ التوكن او بيانات المستخدم (لاحقاً)
-        return true; // نجح الدخول
+        return true;
       } else {
-        if (kDebugMode) {
-          print('Login Error: ${response.body}');
-        }
+        print('Login Error: ${response.body}');
         return false;
       }
     } catch (e) {
-      if (kDebugMode) {
-        print('Error: $e');
-      }
+      print('Connection Error: $e');
       return false;
     }
   }
