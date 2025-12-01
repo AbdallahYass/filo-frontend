c77519e09bdfa08a3fc085b95c1aa0d383e2832f
Abdallahyass <abdallahaahmad2000@gmail.com>
2025-12-01 21:34:33 +0300

    Fix web icons and images

--- lib/services/menu_service.dart
+++ lib/services/menu_service.dart
@@ -5,19 +5,31 @@ import 'package:http/http.dart' as http;
 import '../models/menu_item.dart';
 
 class MenuService {
-  // 👇👇👇 الرابط العالمي الجديد (يعمل على الويب والموبايل)
-  final String baseUrl = 'https://filo-menu.onrender.com/api/menu';
+  String get baseUrl {
+    // نستخدم الرابط العالمي دائماً الآن
+    return 'https://filo-menu.onrender.com/api/menu';
+  }
 
   Future<List<MenuItem>> fetchMenu() async {
     try {
       print("جاري الاتصال بـ: $baseUrl");
-      final response = await http.get(Uri.parse(baseUrl));
+
+      // 👇👇👇 التعديل الهام هنا: إضافة الـ Header
+      final response = await http.get(
+        Uri.parse(baseUrl),
+        headers: {
+          'Content-Type': 'application/json',
+          'x-api-key': 'FiloSecretKey2025', // 🔑 المفتاح السري
+        },
+      );
 
       if (response.statusCode == 200) {
         List<dynamic> body = jsonDecode(response.body);
         return body.map((json) => MenuItem.fromJson(json)).toList();
       } else {
-        throw Exception('خطأ في السيرفر: ${response.statusCode}');
+        throw Exception(
+          'خطأ في السيرفر: ${response.statusCode} - ${response.body}',
+        );
       }
     } catch (e) {
       print("خطأ في الاتصال: $e");
